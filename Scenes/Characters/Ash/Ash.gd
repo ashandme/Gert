4ee38d649fe1bac4b504be1c076648c3e4debef3
ashandme <santiagosoleto121@gmail.com>
2019-07-28 14:11:38 -0300

    28/7 attack complete

--- Scenes/Characters/Ash/Ash.gd
+++ Scenes/Characters/Ash/Ash.gd
@@ -1,17 +1,19 @@
 extends KinematicBody2D
-var Movement = Vector2(); #vec2 get horizontal and Vertical value
-var Gravity = 15;
+#------------------------------------------------------------------------------------------#
+#--------------------------------|declaration of variables|--------------------------------#
+#------------------------------------------------------------------------------------------#
+var Movement = Vector2(); #vec2 gives horizontal and Vertical value
+var Gravity = 13;
 var Jump = -300;
 const FLOOR = Vector2(0 , -1);
 var Speed = 70; 
 var Direction = -1; #get direction for flip sprites
 var OnG = false; #on Ground variable
 var boolAttack = false
-#other
-#life
 var Heart = 5;
 func _physics_process(delta):
 	if Heart > 0:
+		#---|input|---#
 		if Input.is_action_pressed("Right_M"):
 			Movement.x = Speed;
 		elif Input.is_action_pressed("Left_M"):
@@ -23,48 +25,54 @@ func _physics_process(delta):
 				Movement.y = Jump
 		if Input.is_action_just_pressed("Attack_X"):
 			attack()
-		#Get
+		#--------|change the value of OnG if the character is on ground|--------#
 		if is_on_floor():
 			OnG = true;
 		else:
 			OnG = false;
-		#SET DIRECTION
+		#--------|Set Direction|--------#
 		if Movement.x > 0:
 			Direction = 1
 		if Movement.x < 0:
 			Direction = -1
-		#anim
-		if boolAttack == false:
-			if Direction == -1:
-				$Body.set_flip_h(false)
-				$Legs.set_flip_h(false)
-			if Direction == 1:
-				$Body.set_flip_h(true)
-				$Legs.set_flip_h(true)
-			#animG
-			if Movement.x != 0 and OnG:
-				$AshAnim.play("Run")
-			if Movement.x == 0 and OnG:
-				$AshAnim.play("Idle")
-			if Movement.y < 0 and OnG == false:
-				$AshAnim.play("Jump")
-			if Movement.y == 0 and OnG == false:
-				$AshAnim.play("Jump at Fall")
-		else:
-			$AshAnim.play("Attack1")
-		#General Movement
+		if !boolAttack:
+			Body_Animation()
+			Flip_H()
+		Legs_Animation()
 		Movement.y += Gravity;
 		Movement = move_and_slide(Movement, FLOOR);
 	$GUI/TextureProgress.set_value(Heart)
+func Body_Animation():
+	if Movement.x != 0 and OnG:
+		$Body/BodyAnim.play("Run")
+	if Movement.x == 0 and OnG:
+		$Body/BodyAnim.play("Idle")
+	if Movement.y < 0 and !OnG:
+		$Body/BodyAnim.play("Jump")
+	if Movement.y > 0 and !OnG:
+		$Body/BodyAnim.play("Jump at Fall")
+func Legs_Animation():
+	if Movement.x != 0 and OnG:
+		$Legs/LegsAnim.play("Run")
+	if Movement.x == 0 and OnG:
+		$Legs/LegsAnim.play("Idle")
+	if Movement.y < 0 and !OnG:
+		$Legs/LegsAnim.play("Jump")
+	if Movement.y > 0 and !OnG:
+		$Legs/LegsAnim.play("Jump at Fall")		
+func Flip_H():
+	if Direction == -1:
+		$Body.set_flip_h(false)
+		$Legs.set_flip_h(false)
+	if Direction == 1:
+		$Body.set_flip_h(true)
+		$Legs.set_flip_h(true)
 func Damage(Damage):
 	Heart += Damage
-func disableTrack():
-	if boolAttack == true:
-		$AshAnim.get_animation("Idle").track_is_enabled("Body:frame")
-		$AshAnim.find_animation("Run").find_track($Body).track_set_enabled(true)
 func attack():
 	if (!boolAttack):
-		$AshAnim.play("Attack1")
+		$Body/BodyAnim.play("Attack1")
 		boolAttack = true
-		yield($AshAnim,"animation_finished")
+		yield($Body/BodyAnim,"animation_finished")
 		boolAttack = false
+		$Legs/LegsAnim.stop()
